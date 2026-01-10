3aef4bab6c29aab4428b6809f5b56bce4b5712da
Saif Mahmud <msaifofficial@gmail.com>
2026-01-10 17:15:17 -0600

    fix: update apollo app id to correct account

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -74,7 +74,7 @@ export function Analytics() {
             dangerouslySetInnerHTML={{
               __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
 o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
-o.onload=function(){window.trackingFunctions.onLoad({appId:"6940aae5e5599a001db5755c"})},
+o.onload=function(){window.trackingFunctions.onLoad({appId:"6962dcb35184a80019de283f"})},
 document.head.appendChild(o)}initApollo();`,
             }}
           />
